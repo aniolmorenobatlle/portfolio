6fb8bfe697fe1efb2b73e8777fde89b9fab06d41
aniolmorenobatlle <amoreno@cendrassos.net>
2024-03-10 13:41:30 +0100

    Owl Carousel

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// Owl Carousel
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    responsive: {
+        0: {
+            items: 4,
+        },
+        600: {
+            items: 5,
+        },
+        1000: {
+            items: 8,
+        },
+    },
+});
