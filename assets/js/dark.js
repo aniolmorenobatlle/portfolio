a2dbc4769901c1adff9fd3ae589c828bb7186b94
aniolmorenobatlle <amoreno@cendrassos.net>
2024-03-15 15:55:44 +0100

    Multi Idioma

--- assets/js/dark.js
+++
@@ -1,4 +0,0 @@
-function theme(event) {
-    console.log('theme');
-    document.documentElement.classList.toggle('dark');
-}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
+// Dark Mode
+function theme(event) {
+    console.log('theme');
+    document.documentElement.classList.toggle('dark');
+}
+
 // Owl Carousel
 $('.owl-carousel').owlCarousel({
     loop: true,
@@ -15,3 +21,49 @@ $('.owl-carousel').owlCarousel({
         },
     },
 });
+
+// Language Selector
+var currentLanguage = localStorage.getItem('language') || 'ca';
+changeLanguage(currentLanguage);
+
+function changeLanguage(language) {
+    // Carrega les traduccions
+    fetch('translations.json')
+        .then((response) => response.json())
+        .then((translations) => {
+            // Itera sobre els elements amb l'atribut data-translate
+            var elementsToTranslate =
+                document.querySelectorAll('[data-translate]');
+            elementsToTranslate.forEach(function (element) {
+                var key = element.getAttribute('data-translate');
+                element.textContent = translations[language][key];
+            });
+
+            // Actualitza l'idioma actual a localStorage
+            localStorage.setItem('language', language);
+
+            // Actualitza el botó del idioma seleccionat amb la bandera i el nom del idioma
+            var button = document.getElementById('states-button');
+            var flagImg = button.querySelector('img');
+            var languageName = button.querySelector('span');
+
+            switch (language) {
+                case 'ca':
+                    flagImg.src = '/assets/img/languages/catalan_flag.png';
+                    languageName.textContent = 'Català';
+                    break;
+                case 'es':
+                    flagImg.src = '/assets/img/languages/spanish_flag.png';
+                    languageName.textContent = 'Español';
+                    break;
+                case 'en':
+                    flagImg.src = '/assets/img/languages/english_flag.png';
+                    languageName.textContent = 'English';
+                    break;
+                // Añade más casos según los idiomas necesarios
+            }
+        })
+        .catch((error) =>
+            console.error('Error carregant les traduccions:', error),
+        );
+}
